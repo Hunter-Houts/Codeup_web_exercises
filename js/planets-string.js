d3578a529897c7998cad86837e7dbb893efcb545
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-08-15 14:17:20 -0500

    Actually used join in exercise instead of for loop.

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,28 +23,33 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-    var newArry = [];
-    for(var i = 0; i < planetsArray.length; i++){
-        // newArry.push("<br>");
-        newArry.push(planetsArray[i]);
-        newArry.push("<br>");
-    }
-    String.prototype.replaceAll = function(search, replacement) {
-        var target = this;
-        return target.split(search).join(replacement);
-    };
-    newArry = newArry.join(",");
-    newArry = newArry.replaceAll(",", "");
-    console.log(newArry);
-    var ul = [];
-    for(var i = 0; i < planetsArray.length; i++){
-        ul.push("<li>");
-        ul.push(planetsArray[i]);
-        ul.push("</li>");
-    }
-    ul.unshift("<ul>");
-    ul.push("</ul>");
-    ul = ul.join(",");
-    ul = ul.replaceAll(",", "");
-    console.log(ul);
+
+    var planetsWithBr = planetsArray.join("<br>");
+
+    var planetsToList = "<ul><li>" + planetsArray.join("</li><li>") + "</li></ul>";
+    console.log(planetsToList);
+    // var newArry = [];
+    // for(var i = 0; i < planetsArray.length; i++){
+    //     // newArry.push("<br>");
+    //     newArry.push(planetsArray[i]);
+    //     newArry.push("<br>");
+    // }
+    // String.prototype.replaceAll = function(search, replacement) {
+    //     var target = this;
+    //     return target.split(search).join(replacement);
+    // };
+    // newArry = newArry.join(",");
+    // newArry = newArry.replaceAll(",", "");
+    // console.log(newArry);
+    // var ul = [];
+    // for(var i = 0; i < planetsArray.length; i++){
+    //     ul.push("<li>");
+    //     ul.push(planetsArray[i]);
+    //     ul.push("</li>");
+    // }
+    // ul.unshift("<ul>");
+    // ul.push("</ul>");
+    // ul = ul.join(",");
+    // ul = ul.replaceAll(",", "");
+    // console.log(ul);
 })();
