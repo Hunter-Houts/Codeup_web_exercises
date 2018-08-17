3bd0457a9d29d0f82f8c573d809b9aa6292af051
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-08-17 15:35:31 -0500

    Fixed math

--- js/circle.js
+++ js/circle.js
@@ -9,7 +9,7 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return (Math.PI * Math.sqrt(this.radius));
+            return (Math.PI * Math.pow(this.radius, 2));
             /* TODO: return the proper value */
         },
 
